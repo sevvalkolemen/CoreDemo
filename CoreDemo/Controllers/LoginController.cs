18176cce7aa2a0183dab5622e794dce02f5009f3
sevvalkolemen <seval.kolemen@gmail.com>
2022-04-25 23:58:34 +0300

    147. Ders Sonu

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -53,5 +53,9 @@ namespace CoreDemo.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Login");
         }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
